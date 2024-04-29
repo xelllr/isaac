bd3a6d4c528d1fdbc5ce1116544f92e47c6ec462
Axel Riou <axel.riou@etu.univ-nantes.fr~>
2024-04-29 23:33:00 +0200

    Fin du tuto 2. Game clock, gestion touche (le rectangle bouge !!!)

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -11,13 +11,23 @@ public class GamePanel extends javax.swing.JPanel implements Runnable {
     final int screenWidth = tilesSize * maxScreenCol;
     final int screenHeight = tilesSize * maxScreenRow;
 
+    double FPS = 60;
+
+    KeyHandler keyH = new KeyHandler();
     Thread gameThread;
+
+    // position du joueur
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
 
     public GamePanel() {
 
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
     }
 
     public void startGameThread() {
@@ -30,14 +40,30 @@ public class GamePanel extends javax.swing.JPanel implements Runnable {
     @Override
     public void run() {
 
-        while(gameThread != null) {
+        double drawInterval = 1000000000 / FPS;
+        double nextDrawTime = System.nanoTime() + drawInterval;
 
-            // System.out.println("Caca");
+        while(gameThread != null) {
             // On met à jour la position du joueur là dedans par exemple
             update();
 
             // Puis on dessine ces nouvelles infos
             repaint();
+
+            try {
+                double remainingTime = nextDrawTime - System.nanoTime();
+                remainingTime = remainingTime / 1000000;
+
+                if(remainingTime < 0) {
+                    remainingTime = 0;
+                }
+
+                Thread.sleep((long) remainingTime);
+                nextDrawTime += drawInterval;
+
+            } catch (InterruptedException e){
+                e.printStackTrace();
+            }
 
             }
 
@@ -45,11 +71,29 @@ public class GamePanel extends javax.swing.JPanel implements Runnable {
 
     public void update() {
 
+        if(keyH.upPressed) {
+            playerY -= playerSpeed;
+        }
+        else if(keyH.downPressed) {
+            playerY += playerSpeed;
+        }
+        else if(keyH.leftPressed) {
+            playerX -= playerSpeed;
+        }
+        else if(keyH.rightPressed) {
+            playerX += playerSpeed;
+        }
     }
 
     public void paintComponent(Graphics g) { // Grapgics c'est le crayon en gros
 
         super.paintComponent(g);
+
+        Graphics2D g2d = (Graphics2D)g;
+
+        g2d.setColor(Color.white);
+        g2d.fillRect(playerX, playerY, tilesSize, tilesSize);
+        g2d.dispose();
     }
 }
 
--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
    public static void main(String[] args) throws InterruptedException {
 
+      System.setProperty("sun.java2d.opengl", "true");
+
       JFrame window = new JFrame();
       window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
       window.setResizable(false);
